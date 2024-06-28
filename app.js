16cec4c38cf862d7a304edc10d131aebf9e28b65
Anthony Yin <yin.hao@rocketmail.com>
2024-06-28 22:39:11 +1200

    Sprite, shadow, and fix light

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('renderCanvas');
 
 var hk;
 
-var player;
+var playerBody, playerSprite;
 var playerAggregate;
 // Just to test
 var groundAggregate;
@@ -12,7 +12,7 @@ var firstPlatform;
 var canJump = true;
 var canChangeDirection= false;
 
-
+var light;
 
 let createPlatform;
 
@@ -20,10 +20,11 @@ var gameLoop = function( engine )
 {
     engine.runRenderLoop( function() {
 
-        console.log("canJump: ", canJump);
-        console.log("canChangeDirection: ", canChangeDirection);
+        // Update player sprite position based on physics body
+        playerSprite.position.x = playerBody.position.x;
+        playerSprite.position.y = playerBody.position.y;
 
-        if (player.position.y <= firstPlatform.position.y - 1) {
+        if (playerBody.position.y <= firstPlatform.position.y - 1) {
             alert('Game Over');
             engine.stopRenderLoop();
 
@@ -34,26 +35,25 @@ var gameLoop = function( engine )
         hk.onCollisionObservable.add((ev) => {
             if (ev.type === "COLLISION_STARTED" )
             { 
-
-                const playerPosition = player.getAbsolutePosition();
+                const playerPosition = playerBody.getAbsolutePosition();
                 const platformPosition = ev.collidedAgainst.transformNode.getAbsolutePosition();
 
                 // Calculate collision vector from player to platform
                 const collisionVector = platformPosition.subtract(playerPosition);
                 collisionVector.normalize();
 
-                console.log(collisionVector.y);
+                // console.log(collisionVector.y);
 
                 // Check if collision vector points upwards (adjust threshold as needed)
                 if (collisionVector.y < 0)
-                    playerAggregate.body.setLinearVelocity(new BABYLON.Vector3(0, 5, 0), player.getAbsolutePosition());
+                    playerAggregate.body.setLinearVelocity(new BABYLON.Vector3(0, 5, 0), playerBody.getAbsolutePosition());
                     canJump = true;
                 }
         }); 
         
     
         // Generate new platforms as the player ascends
-        if (player.position.y > scene.meshes[scene.meshes.length - 1].position.y - 10) {``
+        if (playerBody.position.y > scene.meshes[scene.meshes.length - 1].position.y - 10) {``
             createPlatform(scene.meshes[scene.meshes.length - 1].position.y + 1);
         }
         if ( gamesScene && gamesScene.activeCamera )
@@ -78,27 +78,39 @@ const createScene = function() {
     scene.enablePhysics(new BABYLON.Vector3(0, -9.8, 0), hk);
 
     // Create a basic light
-    const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+    var light = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(-1, -2, -1), scene);
+	light.position = new BABYLON.Vector3(20, 40, 20);
+	light.intensity = 0.7;
     
-    // Create the player sphere
-    player = BABYLON.MeshBuilder.CreateSphere('player', { diameter: 1 }, scene);
-    player.position.y = 1;
+    // Create the player body sphere
+    playerBody = BABYLON.MeshBuilder.CreateSphere('playerBody', { diameter: 1.0 }, scene);
+    playerBody.isVisible = false;
+    playerBody.position.y = 1;
 
     // Enable physics for the player
-    playerAggregate = new BABYLON.PhysicsAggregate(player, BABYLON.PhysicsShapeType.SPHERE, { mass: 0.2, restitution: 0.5 }, scene);
+    playerAggregate = new BABYLON.PhysicsAggregate(playerBody, BABYLON.PhysicsShapeType.SPHERE, { mass: 0.2, restitution: 0.5 }, scene);
     playerAggregate.body.setCollisionCallbackEnabled(true);
 
+    // Create player sprite and physics body
+    const playerManager = new BABYLON.SpriteManager('playerManager', 'doodle_cute.png', 1, { width: 648, height: 385 }, scene);
+    playerSprite = new BABYLON.Sprite('player', playerManager);
+    playerSprite.width = 1.5;
+    playerSprite.height = 1;
 
     // Create ground
     const ground = BABYLON.MeshBuilder.CreateGround('ground1', { width: 10, height: 10 }, scene);
     ground.position.y = -1;
     groundAggregate = new BABYLON.PhysicsAggregate(ground, BABYLON.PhysicsShapeType.BOX, { mass: 0, restitution: 0.5 }, scene);
 
+    // Shadow generator
+    const shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
+    shadowGenerator.addShadowCaster(playerBody);
+    shadowGenerator.usesExponentialShadowMap = true;
+    ground.receiveShadows = true;
+
     // Create a FollowCamera
     const camera = new BABYLON.FollowCamera('followCamera', new BABYLON.Vector3(0, 10, -10), scene);
-    camera.lockedTarget = player; // Set the target to follow
+    camera.lockedTarget = playerBody; // Set the target to follow
     camera.radius = 10; // Distance from the target
     camera.heightOffset = 5; // Height above the target
     camera.rotationOffset = 0; // Angle around the target
@@ -116,6 +128,7 @@ const createScene = function() {
         platform.position.y = yPosition;
         platform.position.z = 0;
         platform.physicsImpostor = new BABYLON.PhysicsAggregate(platform, BABYLON.PhysicsShapeType.BOX, { mass: 0, restitution: 0.5 }, scene);
+        platform.receiveShadows = true;
         return platform;
     };
 
@@ -130,7 +143,7 @@ const createScene = function() {
     // Function to handle jumping and movement
     window.addEventListener('keydown', function(event) {
         if (event.code === 'Space' && canJump) {
-            playerAggregate.body.setLinearVelocity(new BABYLON.Vector3(0, 10, 0), player.getAbsolutePosition());;
+            playerAggregate.body.setLinearVelocity(new BABYLON.Vector3(0, 10, 0), playerBody.getAbsolutePosition());;
             canJump = false;
         }
     });
@@ -139,16 +152,10 @@ const createScene = function() {
     window.addEventListener('keydown', function(event) {
         if (!canJump) {
             if (event.code === 'ArrowLeft') {
-                playerAggregate.body.applyImpulse(new BABYLON.Vector3(1, 0, 0), player.getAbsolutePosition());
+                playerAggregate.body.applyImpulse(new BABYLON.Vector3(1, 0, 0), playerBody.getAbsolutePosition()); 
             }
             if (event.code === 'ArrowRight') {
-                playerAggregate.body.applyImpulse(new BABYLON.Vector3(-1, 0, 0), player.getAbsolutePosition());
-            }
-            if (event.code === 'ArrowUp') {
-                playerAggregate.body.applyImpulse(new BABYLON.Vector3(0, 0, -3), player.getAbsolutePosition());
-            }
-            if (event.code === 'ArrowDown') {
-                playerAggregate.body.applyImpulse(new BABYLON.Vector3(0, 0, 3), player.getAbsolutePosition());
+                playerAggregate.body.applyImpulse(new BABYLON.Vector3(-1, 0, 0), playerBody.getAbsolutePosition());
             }
         }
     });
